13f1e649b1f5d10b0b126e7d3b69fb6d574a046a
roei <royvagner@gmail.com>
2015-09-12 18:12:09 -0400

    sar iteration 1

---
+++ Algorithm.CSharp/Scalping2Algorithm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using QuantConnect.Data.Consolidators;
+using QuantConnect.Data.Market;
+using QuantConnect.Indicators;
+using QuantConnect.Securities;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    // http://www.dolphintrader.com/forex-scalping-strategy-parabolic-sar-advanced-macd-v3-indicator/
+    public class Scalping2Algorithm : QCAlgorithm
+    {
+        private ExponentialMovingAverage ema;
+        private ParabolicStopAndReversal sar;
+
+
+        public override void Initialize()
+        {
+            SetStartDate(2015, 09, 08);
+            SetEndDate(2015, 09, 08);
+            
+
+            AddSecurity(SecurityType.Forex, "EURUSD", Resolution.Tick, "oanda", true, 0, false);
+
+            ema = EMA("EURUSD", 125);
+            sar = new ParabolicStopAndReversal();
+            RegisterIndicator("EURUSD", sar, Resolution.Minute);
+
+            var consolidator = new TickConsolidator(TimeSpan.FromMinutes(1));
+            consolidator.DataConsolidated += OnMinute;
+            SubscriptionManager.AddConsolidator("EURUSD", consolidator);
+
+            Securities["EURUSD"].TransactionModel = new ConstantFeeTransactionModel(0);
+        }
+
+        public void OnMinute(Object o, TradeBar bar)
+        {
+
+
+            Plot("Price", "close", bar.Close);
+            //Plot("Price", "EMA-125", ema.Current);
+            Plot("Price", "SAR", sar.Current);
+            Plot("Balance", "Balance", Portfolio.TotalPortfolioValue);
+            PlotIndicator("EMA", ema);
+        }
+
+        public bool ChekcLongOpportunity(TradeBar bar)
+        {
+            return false;
+        }
+
+        public void OnData(Ticks data)
+        {
+            
+        }
+    }
+}
---
+++ Indicators/ParabolicStopAndReversal.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using QuantConnect.Data.Market;
+
+namespace QuantConnect.Indicators
+{
+    // http://www.esignalcentral.com/support/futuresource/workstation/help/charts/studies/parab.htm
+    /// <summary>
+    ///     P = Position
+    /// </summary>
+    public class ParabolicStopAndReversal : TradeBarIndicator
+    {
+        private Position _position;
+        private TradeBar _previousBar;
+        private decimal _previousSar;
+        private decimal _previousEP;
+        private bool _isPositionChanged;
+        private bool _isReady;
+        private decimal _previousAF = 0.02m;
+
+
+        public ParabolicStopAndReversal() : base("SAR")
+        {
+        }
+
+        public override bool IsReady
+        {
+            get { return _isReady; }
+        }
+
+        private void Init(TradeBar currentBar)
+        {
+            
+            // init position
+            if (currentBar.Close < _previousBar.Close)
+            {
+                _position = Position.Short;
+            }
+            
+            if (currentBar.Close >= _previousBar.Close)
+            {
+                _position = Position.Long;
+            }
+
+            
+            // init sar
+            if (_position == Position.Short)
+            {
+                _previousSar = _previousBar.High;
+            }
+
+            if (_position == Position.Long)
+            {
+                _previousSar = _previousBar.Low;
+            }
+
+
+            // init Extreme price
+            if (_position == Position.Short)
+            {
+                _previousEP = Math.Min(currentBar.Low, _previousBar.Low);
+            }
+
+            if (_position == Position.Long)
+            {
+                _previousEP = Math.Min(currentBar.High, _previousBar.High);
+            }
+
+            _isReady = false;
+        }
+
+        protected override decimal ComputeNextValue(TradeBar input)
+        {
+            if (_previousBar == null)
+            {
+                _previousBar = input;
+                return 0;
+            }
+
+            if (Samples == 2)
+            {
+                Init(input);
+                return 0;
+            }
+                
+
+            // Determine current Extreme Price (EP)
+            var currentEP = _position == Position.Short ? Math.Min(_previousEP, input.Low) : Math.Max(_previousEP, input.High);
+
+
+            // Determine Acceleration Factor (AF)
+            var currentAF = _previousAF;
+            if (!_isPositionChanged && _previousEP != currentEP && currentAF < 0.2m)
+                currentAF += 0.02m;
+
+            // Calculate SAR
+            var currentSAR = _previousSar + _previousAF*(_previousEP - _previousSar);
+
+
+            // set "previous values" for next iteration
+            _isPositionChanged = false;
+            _previousEP = currentEP;
+            _previousBar = input;
+            _previousSar = currentSAR;
+            _previousAF = currentAF;
+
+            var nextPosition = _position;
+
+            if (_position == Position.Long && currentSAR >= input.Low)
+            {
+                nextPosition = Position.Short;
+                _isPositionChanged = true;
+                _previousSar = _previousEP;
+                _previousEP = input.Low;
+                _previousAF = 0.02m;
+            }
+
+            if (_position == Position.Short && currentSAR <= input.High)
+            {
+                nextPosition = Position.Long;
+                _isPositionChanged = true;
+                _previousSar = _previousEP;
+                _previousEP = input.High;
+                _previousAF = 0.02m;
+            }
+
+            _position = nextPosition;
+
+
+            return currentSAR;
+        }
+
+
+
+
+        private enum Position
+        {
+            Long,
+            Short
+        }
+
+      
+    }
+}
